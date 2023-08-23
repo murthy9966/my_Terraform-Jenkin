b82c833aee77d4fb3464ce225cef6d6ce2ceffe0
murthy9966 <124232205+murthy9966@users.noreply.github.com>
2023-08-23 18:05:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,32 @@
 provider "aws" {
-    region = "us-east-2"  
+  region = "us-east-2"  
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-024e6efaf93d85776" # us-east-2
-  instance_type = "t2.micro"
-  tags = {
-      Name = "TF-Instance"
-  }
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16" 
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24"  #
+  availability_zone = "us-east-2a"  
+}
+
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+resource "aws_route" "my_route" {
+  route_table_id         = aws_route_table.my_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.my_igw.id
+}
+
+resource "aws_route_table_association" "my_subnet_association" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.my_route_table.id
 }
